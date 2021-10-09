1f31bdbc077a35a29da1288fadc322f9710881ce
jake lamers <jakelamers931@gmail.com>
2021-10-09 13:34:52 -0500

    passes tests greedy_assemble_small_test_2_4 and _3

--- HW1/HW1.py
+++ HW1/HW1.py
@@ -15,6 +15,7 @@ def greedy_assemble(reads, min_overlap=0):
     edges = []
     ham_path = []
     super_str = []
+    visited = []
 
     for i in range(len(reads)):
         for j in [x for x in range(len(reads)) if x != i]:
@@ -24,6 +25,44 @@ def greedy_assemble(reads, min_overlap=0):
 
     edges.sort(key=lambda y: (-y[2],reads[y[0]] + reads[y[1]]))
 
+    #print(edges)
+
+    for i in range(len(edges)):
+        edge = edges[i]
+
+        if i == 0:
+            ham_path.append(edge)
+            visited.append(edge[0])
+        else:
+            if sum([edge[0] == i[0] for i in ham_path]) == 0 and sum([edge[1] == i[1] for i in ham_path]) == 0 and sum([edge[1] == i for i in visited]) == 0:
+                ham_path.append(edge)
+                visited.append(edge)
+
+    #print(ham_path)
+
+    for i in range(len(ham_path)):
+        if i < len(ham_path) - 1:
+            super_str.append(reads[ham_path[i][0]])
+        else:
+            super_str.append(reads[ham_path[i][0]])
+            super_str.append(reads[ham_path[i][1]])
+
+    if len(super_str)  > 0:
+        super_str = [''.join(super_str)]
+
+    nodes_in_ham_path = [edge[0] for edge in ham_path]
+    [nodes_in_ham_path.append(edge[1]) for edge in ham_path]
+
+    for i in range(len(reads)):
+        if i not in nodes_in_ham_path:
+            super_str.append(reads[i])
+
+    super_str.sort()
+
+    #print(super_str)
+
     return super_str
 
 print(str(greedy_assemble(["C","A","T","G"])) == str(['ACGT']))
+
+print(str(greedy_assemble(["CGAAG", "ATCGA", "AGAG", "GGG"],4)) == str(['AGAG', 'ATCGA', 'CGAAG', 'GGG']))
